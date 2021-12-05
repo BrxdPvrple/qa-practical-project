b24a1536378f55f7b8372db13aaca35bac3c8f09
brxdpvrple <criley.dev@gmail.com>
2021-12-05 10:48:11 +0000

    script update

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,10 +6,10 @@
 
   python3 -m venv venv
   source venv/bin/activate
-  pip3 install -r test_requirements.txt
+  pip install -r test_requirements.txt
 
 
-for service in frontend class-generator stats-generator calculator
+for service in service1 service2 service3 service4
 do
     python3 -m pytest --pyargs $service --cov-report term-missing --cov=app
     
